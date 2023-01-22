d17125713035d636151ef8622f8c8fcb00627867
Tina Rezanezhad <tinarezanezhaad@gmail.com>
2023-01-22 21:17:32 +0100

    Add test to the not started books

--- src/components/NotStartedBooks/NotStartedBooks.test.js
+++ src/components/NotStartedBooks/NotStartedBooks.test.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import React from "react"
+import { render } from '@testing-library/react';
 import NotStartedBooks from './NotStartedBooks';
 
-describe('<NotStartedBooks />', () => {
-  test('it should mount', () => {
-    render(<NotStartedBooks />);
+describe('NotStartedBooks', () => {
+  it('should not render anything when no books are passed', () => {
+    const { queryByTestId } = render(<NotStartedBooks books={null} />);
+    expect(queryByTestId('not-started-books')).toBeNull();
+  });
 
-    const notStartedBooks = screen.getElementsByClassName("mb-4");
-
-    expect(notStartedBooks).toBeInTheDocument();
+  it('should render the correct number of books with status "to-read"', () => {
+    const books = [
+      { googleID: 1, status: 'to-read' },
+      { googleID: 2, status: 'to-read' },
+      { googleID: 3, status: 'reading' }
+    ];
+    const { getAllByTestId } = render(<NotStartedBooks books={books} />);
   });
 });
